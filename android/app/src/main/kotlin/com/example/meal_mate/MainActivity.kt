a0cbd8c44c700757c026c8eec5aa4674c3b81ec8
Obidkhandev <obidkhandev@gmail.com>
2024-03-19 23:49:02 +0500

    add deault folders

---
+++ android/app/src/main/kotlin/com/example/meal_mate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.meal_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  // await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  //     email: "first_user@gmail.com", password: "first_user1234",
+                  // );
+                },
+                child: Text("Add User"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
